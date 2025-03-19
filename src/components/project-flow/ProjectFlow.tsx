e2d56de2d5b08320b8e2d53e0f1f01e74a73488a
Yoann Masson <yoann.m@mozzaik365.com>
2025-03-19 21:38:43 +0100

    Increase ProjectFlow height

--- src/components/project-flow/ProjectFlow.tsx
+++ src/components/project-flow/ProjectFlow.tsx
@@ -177,7 +177,7 @@ export const ProjectFlow: React.FC<ProjectFlowProps> = ({ project }) => {
   }, [initNodes, initEdges]);
 
   return (
-    <div style={{ width: '100%', height: '90vh' }}>
+    <div style={{ width: '100%', height: '98vh' }}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
